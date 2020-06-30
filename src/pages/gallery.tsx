591bad61e2d75d986af84826bbf3ff329ab2d19e
Antonis Skandalis <ant.skandalis@gmail.com>
2020-06-30 19:58:00 +0300

    feat: Add image gallery and page

---
+++ src/pages/gallery.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import SimpleReactLightbox from 'simple-react-lightbox';
+
+import Header from '../components/Header';
+import Layout from '../components/Layout';
+import { useGallery } from '../hooks/useGallery';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+import ImageGallery from '../components/ImageGallery';
+
+const Gallery = () => {
+  const { datoCmsGallery } = useGallery();
+
+  return (
+    <>
+      <Header
+        heroImage={datoCmsGallery.coverImage.fluid}
+        title={datoCmsGallery.title}
+        subtitle={datoCmsGallery.subtitle}
+        bgFixed
+      />
+      <Layout extraClasses="relative z-30 scroll-smooth">
+        <NavBar withLogo />
+        <section className="mb-8 p-12 z-40">
+          <SimpleReactLightbox>
+            <ImageGallery images={datoCmsGallery.photos} />
+          </SimpleReactLightbox>
+        </section>
+      </Layout>
+      <Footer />
+    </>
+  );
+};
+
+export default Gallery;
